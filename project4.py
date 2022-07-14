af649b0e35b0767a46ac2cc54c386973afee495d
waiee <waiee_z@yahoo.com>
2022-07-14 14:55:26 +0800

    python stopwatch

--- project4.py
+++ project4.py
@@ -1,8 +1,24 @@
-#Stock Price Analysis Project
+Stock Price Analysis Project
 import streamlit as st
 import pandas as pd
 
 print("Hello everyone! Testing my Github.")
 print("This code is written by Waiee Zainol.")
-
 print("Test third commmit.")
+
+
+#Python stopwatch
+import time
+def stopWatch():
+    start_time = time.time()
+    s = 0
+    for i in range(1, n+1):
+        s = s + i
+
+    end_time = time.time()
+    return s, end_time-start_time
+
+#Test
+n = 5
+result = stopWatch()
+print(result)
