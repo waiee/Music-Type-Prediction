77bef6ad0c486fc77916c030294c34821f483baf
waiee <waiee_z@yahoo.com>
2022-07-14 16:21:37 +0800

    python execution time comparison

--- project4.py
+++ project4.py
@@ -1,24 +1,53 @@
 Stock Price Analysis Project
 import streamlit as st
 import pandas as pd
+import timeit
+import time
 
 print("Hello everyone! Testing my Github.")
 print("This code is written by Waiee Zainol.")
 print("Test third commmit.")
 
 
-#Python stopwatch
-import time
-def stopWatch():
-    start_time = time.time()
-    s = 0
-    for i in range(1, n+1):
-        s = s + i
+#Python Execution Time
 
-    end_time = time.time()
-    return s, end_time-start_time
+#Test repeat biasa, while loop & for loop
+print("Example of Python execution time: \n")
+print("Repeat, while loop, and for loop comparison: ")
+print("1.", timeit.timeit(stmt='x=0', setup='', number=1000000))
+print("2.",timeit.timeit(stmt='while x < 1000000: x += 1', setup='x=0', number=1))
+print("3.",timeit.timeit(stmt='for x in range(1000000): pass', setup='', number=1))
 
-#Test
-n = 5
-result = stopWatch()
-print(result)
+# print(timeit.timeit(stmt='x-=0; y=0', setup='', number=1000000))
+# print(timeit.timeit(stmt='while x < 1000000: x += 1; y += 1', setup='x=0; y=0', number=1))
+
+########################################################
+
+# #Repeat 6 times
+# print("")
+# print("Repeat 6 times: ")
+# print(timeit.timeit(stmt='while x < 1000000: x += 1', setup='x=0', number=1))
+# print(timeit.timeit(stmt='while x < 1000000: x += 1', setup='x=0', number=1))
+# print(timeit.timeit(stmt='while x < 1000000: x += 1', setup='x=0', number=1))
+# print(timeit.timeit(stmt='while x < 1000000: x += 1', setup='x=0', number=1))
+# print(timeit.timeit(stmt='while x < 1000000: x += 1', setup='x=0', number=1))
+
+# print(timeit.repeat(stmt='while x < 1000000: x += 1', setup='x=0', repeat=5, number=1))
+
+########################################################
+
+#Function for long pieces of code
+
+#build function
+def test_function():
+    for x  in range(1000000):
+        pass
+
+#handle exception - SyntaxError
+print("")
+print("Long code comparison: ")
+try:
+    print("1.",timeit.timeit(stmt='test_function()', setup='from __main__ import test_function', number=1))
+    print("2.",timeit.timeit(stmt='test_function()', setup='', number=1, globals=globals()))
+except SyntaxError:
+    print("SyntaxError occured.")
