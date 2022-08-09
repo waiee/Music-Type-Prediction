29e8fa3d4d41593ef218e9a2c27fdfcbbaa81337
waiee <waiee_z@yahoo.com>
2022-08-09 22:33:56 +0800

    predict x test

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ music_df = pd.read_csv('music.csv')
 music_df.head()
 X = music_df.drop(columns=['genre'])
 y = music_df['genre']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 #--- DECISION TREE MODEL---
 model = DecisionTreeClassifier()
@@ -18,8 +19,12 @@ model.fit(X,y)
 
 #--- MODEL PERSISTANCE ---
 # jb.dump(model, 'music-recommender.joblib')
-model = jb.load('music-recommender.joblib')
-predictions = model.predict([[21,1]])
+# model = jb.load('music-recommender.joblib')
+
+predictions = model.predict(X_test)
+
+
+# predictions = model.predict([[21,1], [22, 0]])
 predictions
 
 # predictions = model.predict([[21,1],[22,0]])
