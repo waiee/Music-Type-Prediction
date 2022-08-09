a989c5f8725fe922488090476e7d7fb48255d7a4
waiee <waiee_z@yahoo.com>
2022-08-09 22:31:51 +0800

    import train test split

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 # Music Type Prediction Project
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 import joblib as jb
 
 #--- DATAFRAME ---
-# music_df = pd.read_csv('music.csv')
-# music_df.head()
-# X = music_df.drop(columns=['genre'])
-# y = music_df['genre']
+music_df = pd.read_csv('music.csv')
+music_df.head()
+X = music_df.drop(columns=['genre'])
+y = music_df['genre']
 
 #--- DECISION TREE MODEL---
-# model = DecisionTreeClassifier()
+model = DecisionTreeClassifier()
 
 # --- TRAIN MODEL ---
-# model.fit(X,y)
+model.fit(X,y)
 
 #--- MODEL PERSISTANCE ---
 # jb.dump(model, 'music-recommender.joblib')
 model = jb.load('music-recommender.joblib')
 predictions = model.predict([[21,1]])
 predictions
-
-print("Hello World.")
 
 # predictions = model.predict([[21,1],[22,0]])
 # predictions
