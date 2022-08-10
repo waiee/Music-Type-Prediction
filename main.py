9a0def9f724e3af17103dee0d1aa3c1c94ea4d84
waiee <waiee_z@yahoo.com>
2022-08-10 21:28:31 +0800

    import tree

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+form sklearn import tree
 import joblib as jb
 
 #--- DATAFRAME ---
@@ -10,18 +11,18 @@ music_df = pd.read_csv('music.csv')
 music_df.head()
 X = music_df.drop(columns=['genre'])
 y = music_df['genre']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8)
 
 #--- DECISION TREE MODEL---
 model = DecisionTreeClassifier()
 
 # --- TRAIN MODEL ---
 model.fit(X,y)
-predictions = model.predict(X_test)
+# predictions = model.predict(X_test)
 
 #---- FIND ACCURACY ----
-score = accuracy_score(y_test, predictions)
-score
+# score = accuracy_score(y_test, predictions)
+# score
 
 #--- MODEL PERSISTANCE ---
 # jb.dump(model, 'music-recommender.joblib')
